eb6209322e8a5d296e6d9413e08b2838c9c39774
ronick-grammer <glflakcm@gmail.com>
2023-06-26 07:01:03 +0900

    refactor: :recycle: 탭 Rx 이벤트 부분 분리

--- Bunjang/Presentation/ListScene/Views/Tab/TabView.swift
+++ Bunjang/Presentation/ListScene/Views/Tab/TabView.swift
@@ -65,30 +65,3 @@ extension TabView: Bindable {
         }.disposed(by: disposeBag)
     }
 }
-
-extension Reactive where Base: TabView {
-    var tabButtonTapped: Observable<PageInfo> {
-        base.rx.itemSelected
-            .map { 
-                let currentPage = $0.row
-                let previousPage = base.viewModel.pageInfo.prev
-                
-                if base.viewModel.pageInfo.current != currentPage {
-                    base.viewModel.pageInfo.prev = base.viewModel.pageInfo.current
-                    base.viewModel.pageInfo.current = currentPage
-                }
-                
-                return (prev: previousPage, current: currentPage)
-            }
-    }
-    
-    var selectedTab: Binder<PageInfo> {
-        Binder(self.base) { tabView, pageInfo in
-            let prevCell = tabView.cellForItem(at: IndexPath(row: pageInfo.prev, section: 0)) as? TabViewCell
-            prevCell?.changeTitleColor(to: .gray)
-            
-            let currentCell = tabView.cellForItem(at: IndexPath(row: pageInfo.current, section: 0)) as? TabViewCell
-            currentCell?.changeTitleColor(to: .black)
-        }
-    }
-}
